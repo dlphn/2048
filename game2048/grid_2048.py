15ad4b3c3336a603a85807e6281d1b809d827216
Delphine Shi <shi.delphine@gmail.com>
2018-11-15 21:55:49 +0100

    Display the game grid nicely

--- game2048/grid_2048.py
+++ game2048/grid_2048.py
@@ -1,5 +1,59 @@
 import random
 
+THEMES = {
+    "0": {
+        "name": "Default",
+        0: "",
+        2: "2",
+        4: "4",
+        8: "8",
+        16: "16",
+        32: "32",
+        64: "64",
+        128: "128",
+        256: "256",
+        512: "512",
+        1024: "1024",
+        2048: "2048",
+        4096: "4096",
+        8192: "8192"
+    },
+    "1": {
+        "name": "Chemistry",
+        0: "",
+        2: "H",
+        4: "He",
+        8: "Li",
+        16: "Be",
+        32: "B",
+        64: "C",
+        128: "N",
+        256: "O",
+        512: "F",
+        1024: "Ne",
+        2048: "Na",
+        4096: "Mg",
+        8192: "Al"
+    },
+    "2": {
+        "name": "Alphabet",
+        0: "",
+        2: "A",
+        4: "B",
+        8: "C",
+        16: "D",
+        32: "E",
+        64: "F",
+        128: "G",
+        256: "H",
+        512: "I",
+        1024: "J",
+        2048: "K",
+        4096: "L",
+        8192: "M"
+    }
+}
+
 
 def create_grid(n=4):
     """
@@ -91,3 +145,83 @@ def init_game(n=4):
     grid = grid_add_new_tile(grid)
     grid = grid_add_new_tile(grid)
     return grid
+
+
+def grid_to_string(grid):
+    result = ""
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            result += " ==="
+        result += "\n"
+        for j in range(len(grid[i])):
+            result += "| " + str(grid[i][j]) + " "
+        result += "|\n"
+    for j in range(len(grid)):
+        result += " ==="
+    return result
+
+
+def long_value(grid):
+    """
+    :param grid: (list)
+    :return: (int) the length of the longest string in the grid
+    """
+    tiles = get_all_tiles(grid)
+    longest = 0
+    for i in range(len(tiles)):
+        if len(str(tiles[i])) > longest:
+            longest = len(str(tiles[i]))
+    return longest
+
+
+def grid_to_string_with_size(grid, n):
+    result = ""
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            result += " " + "=" * (n + 2)
+        result += "\n"
+        for j in range(len(grid[i])):
+            result += "| " + str(grid[i][j]) + " " * (n - len(str(grid[i][j])) + 1)
+        result += "|\n"
+    for j in range(len(grid)):
+        result += " " + "=" * (n + 2)
+    return result
+
+
+def long_value_with_theme(grid, theme):
+    """
+    :param grid: (list)
+    :param theme: (JSON) theme chosen
+    :return: (int) the length of the longest string in the grid
+    """
+    tiles = get_all_tiles(grid)
+    longest = 0
+    for i in range(len(tiles)):
+        val = theme[tiles[i]]
+        if len(val) > longest:
+            longest = len(val)
+    return longest
+
+
+def grid_to_string_with_size_and_theme(grid, theme, n=4):
+    """
+    Display the game grid nicely
+    :param grid: (list)
+    :param theme: (JSON) selected theme
+    :param n: (int) grid size
+    :return: (string) the displayed grid
+    """
+    longest = long_value_with_theme(grid, theme)
+    result = ""
+    for i in range(n):
+        for j in range(n):
+            result += "=" * (longest + 1)
+        result += "=\n"
+        for j in range(n):
+            val = theme[grid[i][j]]
+            result += "|" + val + " " * (longest - len(val))
+        result += "|\n"
+    for j in range(n):
+        result += "=" * (longest + 1)
+    result += "=\n"
+    return result
--- game2048/test_grid_2048.py
+++ game2048/test_grid_2048.py
@@ -2,6 +2,61 @@ from game2048.grid_2048 import *
 from pytest import *
 
 
+THEMES = {
+    "0": {
+        "name": "Default",
+        0: "",
+        2: "2",
+        4: "4",
+        8: "8",
+        16: "16",
+        32: "32",
+        64: "64",
+        128: "128",
+        256: "256",
+        512: "512",
+        1024: "1024",
+        2048: "2048",
+        4096: "4096",
+        8192: "8192"
+    },
+    "1": {
+        "name": "Chemistry",
+        0: "",
+        2: "H",
+        4: "He",
+        8: "Li",
+        16: "Be",
+        32: "B",
+        64: "C",
+        128: "N",
+        256: "O",
+        512: "F",
+        1024: "Ne",
+        2048: "Na",
+        4096: "Mg",
+        8192: "Al"
+    },
+    "2": {
+        "name": "Alphabet",
+        0: "",
+        2: "A",
+        4: "B",
+        8: "C",
+        16: "D",
+        32: "E",
+        64: "F",
+        128: "G",
+        256: "H",
+        512: "I",
+        1024: "J",
+        2048: "K",
+        4096: "L",
+        8192: "M"
+    }
+}
+
+
 def test_create_grid():
     assert create_grid(4) == [[' ', ' ', ' ', ' '], [' ', ' ', ' ', ' '], [' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ']]
 
@@ -53,6 +108,64 @@ def test_init_game():
     assert len(get_empty_tiles_positions(grid)) == 14
 
 
+def test_grid_to_string():
+    a = """
+ === === === ===
+|   |   |   |   |
+ === === === ===
+|   |   |   |   |
+ === === === ===
+|   |   |   |   |
+ === === === ===
+| 2 |   |   | 2 |
+ === === === ===
+    """
+    grid_to_string([[' ', ' ', ' ', ' '], [' ', ' ', ' ', ' '], [' ', ' ', ' ', ' '], [2, ' ', ' ', 2]]) == a
+
+
+def test_grid_to_string_with_size():
+    a = """
+ ====== ====== ====== ======
+|      |      |      |      |
+ ====== ====== ====== ======
+|      |      |      |      |
+ ====== ====== ====== ======
+|      |      |      |      |
+ ====== ====== ====== ======
+| 2048 |      |      | 2    |
+ ====== ====== ====== ======
+    """
+    grid = [[' ', ' ', ' ', ' '], [' ', ' ', ' ', ' '], [' ', ' ', ' ', ' '], [2048, ' ', ' ', 2]]
+    grid_to_string_with_size(grid, long_value(grid)) == a
+
+
+def test_long_value_with_theme():
+    grid = [[2048, 16, 32, 0], [0, 4, 0, 2], [0, 0, 0, 32], [512, 1024, 0, 2]]
+    assert long_value_with_theme(grid, THEMES["0"]) == 4
+    assert long_value_with_theme(grid, THEMES["1"]) == 2
+    assert long_value_with_theme(grid, THEMES["2"]) == 1
+    grid = [[16, 4, 8, 2], [2, 4, 2, 128], [4, 512, 32, 4096], [1024, 2048, 512, 2]]
+    assert long_value_with_theme(grid, THEMES["0"]) == 4
+    assert long_value_with_theme(grid, THEMES["1"]) == 2
+    assert long_value_with_theme(grid, THEMES["2"]) == 1
+
+
+def test_grid_to_string_with_size_and_theme():
+    grid = [[16, 4, 8, 2], [2, 4, 2, 128], [4, 512, 32, 64], [1024, 2048, 512, 2]]
+    a = """
+=============
+|Be|He|Li|H |
+=============
+|H |He|H |N |
+=============
+|He|F |B |C |
+=============
+|Ne|Na|F |H |
+=============
+"""
+    assert grid_to_string_with_size_and_theme(grid, THEMES["1"], 4) == a[1:]
+
+
 test_create_grid()
 test_grid_add_new_tile_at_position()
 test_get_value_new_tile()
@@ -61,3 +174,7 @@ test_get_empty_tiles_positions()
 test_get_new_position()
 test_grid_add_new_tile()
 test_init_game()
+test_grid_to_string()
+test_grid_to_string_with_size()
+test_long_value_with_theme()
+test_grid_to_string_with_size_and_theme()
