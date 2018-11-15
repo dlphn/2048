7bbea69a0382ec9d51eae189c66cc505936eca8b
Delphine Shi <shi.delphine@gmail.com>
2018-11-15 21:18:12 +0100

    Add comments

--- game2048/grid_2048.py
+++ game2048/grid_2048.py
@@ -1,10 +1,16 @@
 import random
 
 
-def create_grid(size):
+def create_grid(n=4):
+    """
+    Create a new game grid
+    :param n: (int) the size of the grid nxn
+    :return: (list) the grid as a list of lists
+    """
     game_grid = []
-    for i in range(0, size):
-        line = [' '] * size
+    # Grid of size n x n
+    for i in range(0, n):
+        line = [' '] * n
         game_grid.append(line)
     return game_grid
 
@@ -27,6 +33,10 @@ def get_all_tiles(grid):
 
 
 def get_value_new_tile():
+    """
+    Randomly pick the value of a new tile: 2 with probability 90%, 4 with probability 10%.
+    :return: (int) the random value chosen
+    """
     possible_values = [2] * 9 + [4]
     rand_value = random.choice(possible_values)
     return rand_value
@@ -44,6 +54,11 @@ def get_empty_tiles_positions(grid):
 
 
 def get_new_position(grid):
+    """
+    Pick a random empty position
+    :param grid: (list) the game grid as a list of lists
+    :return: the position (x, y)
+    """
     pos = get_empty_tiles_positions(grid)
     rand = random.randint(0, len(pos) - 1)
     return pos[rand]
@@ -56,13 +71,23 @@ def grid_get_value(grid, x, y):
 
 
 def grid_add_new_tile(grid):
+    """
+    Pick a random available position and add a new tile 2 or 4 at that position
+    :param grid: (list) the game grid as a list of lists
+    :return: (list) the game grid as a list of lists
+    """
     x, y = get_new_position(grid)
     new_grid = grid_add_new_tile_at_position(grid, x, y)
     return new_grid
 
 
-def init_game(size):
-    grid = create_grid(size)
-    for i in range(2):
-        grid = grid_add_new_tile(grid)
+def init_game(n=4):
+    """
+    Create a new game grid with 2 tiles
+    :param n: (int) the size of the grid
+    :return: (list) the game grid as a list of lists
+    """
+    grid = create_grid(n)
+    grid = grid_add_new_tile(grid)
+    grid = grid_add_new_tile(grid)
     return grid
